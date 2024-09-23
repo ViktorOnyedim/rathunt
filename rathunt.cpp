adee6915253775e304291089f5e1340faf21cf92
ViktorOnyedim <viktoronyedim@gmail.com>
2024-09-23 12:34:36 +0100

    Defined implemented castRay function

--- rathunt.cpp
+++ rathunt.cpp
@@ -28,7 +28,78 @@ double playerX = 1.5;
 double playerY = 1.5;
 double angle = 0; // camera angle
 
+void castRay(SDL_Renderer* renderer, double rayAngle, int rayNum) {
+    double rayDirX = cos(rayAngle);
+	double rayDirY = sin(rayAngle);
 
+    int mapX = floor(playerX);
+	int mapY = floor(playerY);
+
+    double sideDistX;
+	double sideDistY;
+
+    double deltaDistX = std::abs(1 / rayDirX);
+	double deltaDistY = std::abs(1 / rayDirY);
+	double perpWallDist;
+
+    int stepX, stepY;
+    int hitWall = 0;
+    int side;
+
+    if (rayDirX < 0) {
+		stepX = -1;
+		sideDistX = (playerX - mapX) * deltaDistX;
+	} else {
+		stepX = 1;
+		sideDistX = (mapX + 1.0 - playerX) * deltaDistX;
+	}
+
+	if (rayDirY < 0) {
+        stepY = -1;
+        sideDistY = (playerY - mapY) * deltaDistY;
+    } else {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - playerY) * deltaDistY;
+    }
+
+    while (hitWall == 0) {
+        if (sideDistX < sideDistY) {
+            sideDistX += deltaDistX;
+            mapX += stepX;
+            side = 0;
+        } else {
+            sideDistY += deltaDistY;
+            mapY += stepY;
+            side = 1;
+        }
+        if (map[mapY][mapX] > 0) hitWall = 1;
+    }
+    if (side == 0) {
+        perpWallDist = (mapX - playerX + (1 - stepX) / 2) / rayDirX;
+    } else {
+        perpWallDist = (mapY - playerY + (1 - stepY) / 2) / rayDirY;
+    }
+
+    int lineHeight = static_cast<int>(SCREEN_HEIGHT / perpWallDist);
+    int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
+    if (drawStart < 0) drawStart = 0;
+    int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+    if (drawEnd >= SCREEN_HEIGHT) drawEnd = SCREEN_HEIGHT - 1;
+
+    // Drawing wall
+    Uint8 wallColor = static_cast<Uint8>(255 / (1 + perpWallDist * perpWallDist * 0.1));
+    
+    Uint8 r = 3; 
+    Uint8 g = 3; 
+    Uint8 b = 3; 
+    int y1 = 0;
+    int y2 = drawStart;
+
+    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+    SDL_RenderDrawLine(renderer, rayNum, y1, rayNum, y2);
+
+    
+}
 
 
 
@@ -47,23 +118,30 @@ int main( int argc, char* args[] )
     }     else
     {
         //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        window = SDL_CreateWindow( "Rat Hunt", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         if( window == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         } else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
             
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+            SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+            SDL_Event e;
+            bool quit = false;
+            while( quit == false ) { 
+                while( SDL_PollEvent( &e ) ) { 
+                    if( e.type == SDL_QUIT ) quit = true; 
+                }
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+
+                for (int x = 0; x < NUM_RAYS; x++) {
+                    double rayAngle = angle - FOV / 2 + FOV * x / static_cast<double>(NUM_RAYS);
+                    castRay(renderer, rayAngle, x);
+                }
+            }
         }
     }
     //Destroy window
